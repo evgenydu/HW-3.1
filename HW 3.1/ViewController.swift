d78ef8ab715bfe9028efb44a8fc56dcbcde10c6e
Evgeny D <ktrp@me.com>
2022-11-02 18:07:32 +0300

    new HW

--- HW 3.1/ViewController.swift
+++ HW 3.1/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var coreAnimationView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startCoreAnimation(_ sender: UIButton) {
+    }
+    
 }
 
